1815eae002e2b7d904f7373983b5479c7fbf5ca1
SrilankaDyingDark <146609015+SrilankaDyingDark@users.noreply.github.com>
2025-05-23 13:40:54 +0800

    Main Menu's music and sounds

--- Scenes/Menus/main_menu.gd
+++ Scenes/Menus/main_menu.gd
@@ -2,15 +2,20 @@ extends Control
 
 signal start_game()
 @onready var buttons_v_box: VBoxContainer = %ButtonsVBox
+@onready var menu_move_sound: AudioStreamPlayer2D = $MenuMoveSound
+@onready var menu_select_sound: AudioStreamPlayer2D = $MenuSelectSound
 
 func _ready() -> void:
 	focus_button()
+	connect_button_signals()
 
 func _on_start_game_button_pressed() -> void:
+	menu_select_sound.play()
 	start_game.emit()
 	#hide()
 
 func _on_quit_button_pressed() -> void:
+	menu_select_sound.play()
 	get_tree().quit()
 
 func _on_visibility_changed() -> void:
@@ -22,3 +27,12 @@ func focus_button() -> void:
 		var button: Button = buttons_v_box.get_child(0)
 		if button is Button:
 			button. grab_focus()
+
+func connect_button_signals():
+	for i in buttons_v_box.get_children():
+		if i is Button:
+			i.focus_entered.connect(_on_button_focus_entered.bind(i))
+
+func _on_button_focus_entered(button: Button) -> void:
+	# 播放移动音效
+	menu_move_sound.play()
